ee283921403a6e936fa7960c0ec7f92639af82c0
Ricardo Gatgens <gatgens48@gmail.com>
2022-03-15 22:21:01 -0600

    Prueba servo

--- Hardware/Hardware.ino
+++ Hardware/Hardware.ino
@@ -2,8 +2,17 @@
 // ls -l /dev/ttyACM*
 // sudo chmod a+rw /dev/ttyACM0
 
+#include <Servo.h>
+
 int input_value;
+
+Servo servoHorizontal;
+
 void setup() {
+  servoHorizontal.attach(9);
+
+
+  
   pinMode(8,  OUTPUT);
   digitalWrite(8, LOW);
   
@@ -12,14 +21,25 @@ void setup() {
 }
 
 void loop() {
-  while (!Serial.available()); // No hace nada hasta que no se abra el puerto serial
-  input_value = Serial.readString().toInt(); // Lee el valor recibido y lo convierte a int
-  if(input_value == 1){
-    led_on();
-  }else{
-    led_off();
-  }
+  //alternarDireccion(servoHorizontal, 750);
 }
+
+void alternarDireccion(Servo servo, int tiempoDeGiro){
+  servo.write(0);
+  delay(tiempoDeGiro);
+  servo.write(180);
+  delay(tiempoDeGiro);
+}
+
+
+
+
+
+
+
+
+
+
 
 void led_on(){
   digitalWrite(8, HIGH);
@@ -27,4 +47,14 @@ void led_on(){
 
 void led_off(){
   digitalWrite(8, LOW);
+}
+
+void controlarLed(){
+  while (!Serial.available()); // No hace nada hasta que no se abra el puerto serial
+  input_value = Serial.readString().toInt(); // Lee el valor recibido y lo convierte a int
+  if(input_value == 1){
+    led_on();
+  }else{
+    led_off();
+  }
 }
