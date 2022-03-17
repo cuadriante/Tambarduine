32daa0f8f63ecd33193f5f9a476424f6d0625e44
Ricardo Gatgens <gatgens48@gmail.com>
2022-03-17 16:12:04 -0600

    pruebas

--- src/prueba1.py
+++ src/prueba1.py
@@ -1,32 +1,46 @@
+from pyparsing import line
 from parser import *
 from lexer import *
 
-codigo = open('prueba.txt')
-lineas = codigo.readlines()
+parser = yacc.yacc()
 
 # code = []
 
 # code.append("SET @var1, True;")
-# code.append("@var1")
-# code.append("SET @var1.T;")
-# code.append("@var1")
-# code.append("SET @var2, 7-3;")
-# code.append("SET @234, 8 @luz, 56;")
-# code.append("@234")
-# code.append("@luz")
-# code.append("@var2")
-# code.append("if 1 < 7 {1 } else {0}")
-# code.append("if 10 < 7 {1 } else {0}")
-# code.append("if 4 < 7 {1 }")
-# code.append("for @var1 to @var1 Step 4 { 5}")
+# code.append("SET @var1.Neg;")
+# # code.append("@var1")
+# # code.append("@var1")
+# # code.append("SET @var2, 7-3;")
+# # code.append("SET @234, 8 @luz, 56;")
+# # code.append("@234")
+# # code.append("@luz")
+# # code.append("@var2")
+# # code.append("if 1 < 7 {1 } else {0}")
+# # code.append("if 10 < 7 {1 } else {0}")
+# # code.append("if 4 < 7 {1 }")
+# # code.append("for @var1 to @var1 Step 4 { 5}")
 
-# print_lexer(texto)
-# code.append(texto)
+# # print_lexer(texto)
+# # code.append(texto)
 
-parser = yacc.yacc()
+# for i in code:
+#     print_lexer(i)
+#     # result = parser.parse(i)
+#     # print(result)
 
-for linea in lineas:
-    print(linea)
-    # print_lexer(i)
-    result = parser.parse(linea)
+
+'''Hay que correr la prueba desde src para que encuentra prueba.txt'''
+
+archivo = open('prueba.txt')
+codigo = archivo.read()
+instrucciones = codigo.split(';')
+
+# print_lexer(codigo)
+# print(parser.parse(codigo))
+
+for i in range(len(instrucciones) - 1):
+    instruccion = instrucciones[i] + ';'
+    print(instruccion)
+    # print_lexer(instruccion)
+    result = parser.parse(instruccion)
     print(result)
