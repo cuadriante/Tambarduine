aa477e25fb0e6d503c84f0f257ec890af18038d1
Yorbre25 <62673860+Yorbre25@users.noreply.github.com>
2022-03-16 21:23:41 -0600

    If, Elseif, For funcionan correctamente

--- src/prueba1.py
+++ src/prueba1.py
@@ -4,19 +4,17 @@ from lexer import *
 code = []
 
 
-code.append("SET @var1.Neg;")
+# code.append("SET @var1.Neg;")
 # code.append("SET @var1, true;")
-# code.append("SET @var2, 7-3;")
 # code.append("SET @234, 8 @luz, 56;")
 # code.append("@234")
 # code.append("@luz")
 # code.append("@var1")
-# code.append("@var2")
-#code.append("if 1 < 7 {1 } else {0}")
-#code.append("for @var1 to @var1 Step { 5}")
+# code.append("if 1 < 7 {1 } else {0}")
+# code.append("if 10 < 7 {1 } else {0}")
+# code.append("if 4 < 7 {1 }")
+code.append("for @var1 to @var1 Step 4 { 5}")
 
-
-texto = "for @var1 to @var1 Step { 5}"
 
 # print_lexer(texto)
 # code.append(texto)
@@ -24,6 +22,6 @@ texto = "for @var1 to @var1 Step { 5}"
 parser = yacc.yacc()
 
 for i in code:
-    # print_lexer(i)
+    print_lexer(i)
     result = parser.parse(i)
     print(result)
